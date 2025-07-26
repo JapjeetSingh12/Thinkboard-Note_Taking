971a8c009aef758d64333bccb080f0aa33cb950e
Japjeet <japjeet@Xubuntu.myguest.virtualbox.org>
2025-07-26 12:27:46 +0530

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,47 @@
-import express from "express"
-import dotenv from "dotenv" ;
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import notesRoutes from "./routes/notesRoutes.js"
+import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors(
-   { origin : "http://localhost:5173"}
-))
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-app.use(express.json());
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
-app.use(rateLimiter)
+app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-app.use("/api/notes" , notesRoutes);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-
-app.listen(5001 , () => {
+  app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
-});
+  });
 });
